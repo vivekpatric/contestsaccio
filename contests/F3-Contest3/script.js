54c29ac8fa7dcf9aab2447caa6b8ff9959249361
vivekpatric <patricleo223@gmail.com>
2023-07-01 23:22:01 +0530

    Contest-3

---
+++ contests/F3-Contest3/script.js
@@ -0,0 +1,11 @@
+// Add event listener for the Login button
+document.querySelector("#login-btn").addEventListener("click", ()=>{
+    // Redirect to login page
+    window.location.href="./login/index.html"
+})
+
+// Add event listener for the Signup button
+document.querySelector("#signup-btn").addEventListener("click", ()=>{
+    // Redirect to signup page
+    window.location.href="./signup/index.html"
+})
